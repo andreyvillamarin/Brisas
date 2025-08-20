86a8bd2e79485693da077ca4ecd69fcc62c72d4a
andreyvillamarin <andreyvillamarin@gmail.com>
2025-08-20 15:08:17 -0500

    actualizacion de titulo
    actualizacion de titulo

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $settings = $settingModel->getAllAsAssoc();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Brisas - Catálogo de Pedidos</title>
+    <title>Las Brisas - Catálogo de Pedidos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
     <?php if (!empty($settings['logo_backend_url'])): ?>
